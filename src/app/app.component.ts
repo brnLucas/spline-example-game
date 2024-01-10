ccd1fe373c4283748f11526520afc9e79b103c47
Breno Nascimento <breno.nascimento@fpf.br>
2024-01-10 19:00:12 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {Application} from '@splinetool/runtime';
+
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spline_game';
+
+  public ngOnInit() {
+    const canvas: any = document.getElementById('canvas3d');
+    const app = new Application(canvas);
+    app.load('assets/mini_home_conditional_logic_copy.spline');
+  }
 }
+
+
+
+
